dd434d62b9ef870513e65c5ab5583c3979d4c9c1
Graham Pash <gtpash96@gmail.com>
2024-03-28 11:34:19 -0500

    work on primal dual tv formulation

--- hippylib/modeling/nonsmoothPrior.py
+++ hippylib/modeling/nonsmoothPrior.py
@@ -22,18 +22,21 @@ import numpy as np
 from ..algorithms.linSolvers import PETScKrylovSolver
 
 class TVPrior:
+    # [1] Chan, Tony F., Gene H. Golub, and Pep Mulet. "A nonlinear primal-dual method for total variation-based image restoration." SIAM journal on scientific computing 20.6 (1999): 1964-1977.
     # primal-dual implementation for (vector) total variation prior
-    def __init__(self, Vhm:dl.FunctionSpace, Vhw:dl.FunctionSpace, alpha:float, beta:float, rel_tol:float=1e-12, max_iter:int=100):
+    def __init__(self, Vhm:dl.FunctionSpace, Vhw:dl.FunctionSpace, Vhwnorm:dl.FunctionSpace, alpha:float, beta:float, rel_tol:float=1e-12, max_iter:int=100):
         self.alpha = dl.Constant(alpha)
         self.beta = dl.Constant(beta)
-        self.Vhm = Vhm
-        self.Vhw = Vhw
+        
+        self.Vhm = Vhm  # function space for the parameter
+        self.Vhw = Vhw  # function space for the slack variable
+        self.Vwnorm = Vhwnorm  # function space for the norm of the slack variable
 
         # assemble mass matrix for parameter
-        mtrial = dl.TrialFunction(Vhm)
-        mtest  = dl.TestFunction(Vhm)
+        self.m_hat = dl.TrialFunction(Vhm)
+        self.m_tilde  = dl.TestFunction(Vhm)
         
-        varfM = ufl.inner(mtrial, mtest)*ufl.dx
+        varfM = ufl.inner(self.m_hat, self.m_tilde)*ufl.dx
         self.M = dl.assemble(varfM)
         self.Msolver = PETScKrylovSolver(self.Vh.mesh().mpi_comm(), "cg", "jacobi")
         self.Msolver.set_operator(self.M)
@@ -43,10 +46,10 @@ class TVPrior:
         self.Msolver.parameters["nonzero_initial_guess"] = False
         
         # assemble mass matrix for slack variable
-        wtrial = dl.TrialFunction(Vhm)
-        wtest  = dl.TestFunction(Vhm)
+        self.w_hat = dl.TrialFunction(Vhw)
+        self.w_tilde  = dl.TestFunction(Vhw)
         
-        varfMw = ufl.inner(wtrial, wtest)*ufl.dx
+        varfMw = ufl.inner(self.w_hat, self.w_tilde)*ufl.dx
         self.Mw = dl.assemble(varfMw)
         self.Mwsolver = PETScKrylovSolver(self.Vhw.mesh().mpi_comm(), "cg", "jacobi")
         self.Mwsolver.set_operator(self.M)
@@ -69,11 +72,47 @@ class TVPrior:
     
     def cost(self, m):
         # (smoothed) TV functional
-        return dl.sqrt( dl.inner(dl.grad(m), dl.grad(m)) + self.beta)*dl.dx
+        return self.alpha * dl.sqrt( dl.inner(dl.grad(m), dl.grad(m)) + self.beta)*dl.dx
     
     
     def grad(self, m, out):
-        raise NotImplementedError("Gradient not implemented.")
+        TVm = self._fTV(m)
+        grad_tv = self.alpha * dl.Constant(1.)/TVm*dl.inner(dl.grad(m), dl.grad(self.m_tilde))*dl.dx
+        
+        # assemble the UFL form to a vector, add to out
+        v = dl.assemble(grad_tv)
+        out.axpy(1., v)
+    
+    
+    def hess(self, m, w):
+        Tvm = self._fTV(m)
+        
+        # symmetrized version of (5.2) from [1]
+        A = dl.Constant(1.)/Tvm * ( dl.Identity(2) 
+                                    - dl.Constant(0.5)*dl.outer(w, dl.grad(m)/Tvm)
+                                    - dl.Constant(0.5)*dl.outer(dl.grad(m)/Tvm, w) )
+        
+        return self.alpha * dl.inner(A*dl.grad(self.m_tilde), dl.grad(self.m_hat))*dl.dx
+    
+    
+    def compute_w_hat(self, m, w, m_hat):
+        Tvm = self._fTV(m)
+        
+        # symmetrized version of operator in (3.6) from [1]
+        A = dl.Constant(1.)/Tvm * ( dl.Identity(2) 
+                                   - dl.Constant(0.5)*dl.outer(w, dl.grad(m)/Tvm)
+                                   - dl.Constant(0.5)*dl.outer(dl.grad(m)/Tvm, w) )
+        
+        # expression for incremental slack variable (3.6) from [1]
+        dw = A*dl.grad(m_hat) - w + dl.grad(m)/Tvm
+        
+        # todo: project into appropriate space
+        out = dl.Vector()
+        # return self.Mwsolver.solve
+    
+    
+    def wnorm(self, w):
+        return dl.inner(w, w)
     
     
 class TVGaussianPrior:

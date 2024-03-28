d8b4614888df2c08cccf672d95903ad2808a666d
Graham Pash <gtpash96@gmail.com>
2024-03-28 12:53:04 -0500

    w hat computation

--- hippylib/modeling/nonsmoothPrior.py
+++ hippylib/modeling/nonsmoothPrior.py
@@ -22,7 +22,9 @@ import numpy as np
 from ..algorithms.linSolvers import PETScKrylovSolver
 
 class TVPrior:
+    
     # [1] Chan, Tony F., Gene H. Golub, and Pep Mulet. "A nonlinear primal-dual method for total variation-based image restoration." SIAM journal on scientific computing 20.6 (1999): 1964-1977.
+    
     # primal-dual implementation for (vector) total variation prior
     def __init__(self, Vhm:dl.FunctionSpace, Vhw:dl.FunctionSpace, Vhwnorm:dl.FunctionSpace, alpha:float, beta:float, rel_tol:float=1e-12, max_iter:int=100):
         self.alpha = dl.Constant(alpha)
@@ -49,17 +51,17 @@ class TVPrior:
         self.w_hat = dl.TrialFunction(Vhw)
         self.w_tilde  = dl.TestFunction(Vhw)
         
-        varfMw = ufl.inner(self.w_hat, self.w_tilde)*ufl.dx
+        varfMw = dl.inner(self.w_hat, self.w_tilde)*dl.dx
         self.Mw = dl.assemble(varfMw)
         self.Mwsolver = PETScKrylovSolver(self.Vhw.mesh().mpi_comm(), "cg", "jacobi")
-        self.Mwsolver.set_operator(self.M)
+        self.Mwsolver.set_operator(self.Mw)
         self.Mwsolver.parameters["maximum_iterations"] = max_iter
         self.Mwsolver.parameters["relative_tolerance"] = rel_tol
         self.Mwsolver.parameters["error_on_nonconvergence"] = True
         self.Mwsolver.parameters["nonzero_initial_guess"] = False
 
     def _fTV(self, m:dl.Function)->dl.Function:
-        """Helper function to compute the TV norm of a function m.
+        """Computes the TV functional.
 
         Args:
             m (dl.Function): Function to compute the TV norm of.
@@ -85,33 +87,37 @@ class TVPrior:
     
     
     def hess(self, m, w):
-        Tvm = self._fTV(m)
+        TVm = self._fTV(m)
         
         # symmetrized version of (5.2) from [1]
-        A = dl.Constant(1.)/Tvm * ( dl.Identity(2) 
-                                    - dl.Constant(0.5)*dl.outer(w, dl.grad(m)/Tvm)
-                                    - dl.Constant(0.5)*dl.outer(dl.grad(m)/Tvm, w) )
+        A = dl.Constant(1.)/TVm * ( dl.Identity(2) 
+                                    - dl.Constant(0.5)*dl.outer(w, dl.grad(m)/TVm)
+                                    - dl.Constant(0.5)*dl.outer(dl.grad(m)/TVm, w) )
         
         return self.alpha * dl.inner(A*dl.grad(self.m_tilde), dl.grad(self.m_hat))*dl.dx
     
     
     def compute_w_hat(self, m, w, m_hat):
-        Tvm = self._fTV(m)
+        TVm = self._fTV(m)
         
         # symmetrized version of operator in (3.6) from [1]
-        A = dl.Constant(1.)/Tvm * ( dl.Identity(2) 
-                                   - dl.Constant(0.5)*dl.outer(w, dl.grad(m)/Tvm)
-                                   - dl.Constant(0.5)*dl.outer(dl.grad(m)/Tvm, w) )
+        A = dl.Constant(1.)/TVm * ( dl.Identity(2) 
+                                   - dl.Constant(0.5)*dl.outer(w, dl.grad(m)/TVm)
+                                   - dl.Constant(0.5)*dl.outer(dl.grad(m)/TVm, w) )
         
         # expression for incremental slack variable (3.6) from [1]
-        dw = A*dl.grad(m_hat) - w + dl.grad(m)/Tvm
+        dw = A*dl.grad(m_hat) - w + dl.grad(m)/TVm
+        dw = dl.assemble( dl.inner(self.w_tilde, dw)*dl.dx )
         
-        # todo: project into appropriate space
-        out = dl.Vector()
-        # return self.Mwsolver.solve
+        # project into appropriate space
+        out = dl.Vector(self.Mw.mpi_comm())
+        self.Mw.init_vector(out, dw)
+        
+        return out
     
     
     def wnorm(self, w):
+        # todo: assembly, projection
         return dl.inner(w, w)
     
     

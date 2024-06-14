af7bd1fbb64e993fd62b9123f5a46ddae4ab55f4
Graham Pash <gtpash96@gmail.com>
2024-06-14 13:53:45 -0500

    license

--- hippylib/algorithms/SNES.py
+++ hippylib/algorithms/SNES.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2016-2018, The University of Texas at Austin 
+# & University of California--Merced.
+# Copyright (c) 2019-2022, The University of Texas at Austin 
+# University of California--Merced, Washington University in St. Louis.
+# Copyright (c) 2023-2024, The University of Texas at Austin 
+# & University of California--Merced.
+#
+# All Rights reserved.
+# See file COPYRIGHT for details.
+#
+# This file is part of the hIPPYlib library. For more information and source code
+# availability see https://hippylib.github.io.
+#
+# hIPPYlib is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License (as published by the Free
+# Software Foundation) version 2.0 dated June 1991.
+
 import dolfin as dl
 import ufl
 from petsc4py import PETSc
